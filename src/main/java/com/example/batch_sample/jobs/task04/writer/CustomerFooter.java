ab3d553ee3394559a85fc674c9c0501342cdcc7a
HongYeseul <ghd8119@naver.com>
2024-10-26 20:31:42 +0900

    [4회차] csv 파일을 이용해 FlatFileItemWriter 사용해보기 - Footer

---
+++ src/main/java/com/example/batch_sample/jobs/task04/writer/CustomerFooter.java
@@ -0,0 +1,25 @@
+package com.example.batch_sample.jobs.task04.writer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+public class CustomerFooter implements FlatFileFooterCallback {
+    // 결과를 집계하여 총 고객수와 총 나이를 출력
+    ConcurrentHashMap<String, Integer> aggregateCustomers;
+
+    public CustomerFooter(ConcurrentHashMap<String, Integer> aggregateCustomers) {
+        this.aggregateCustomers = aggregateCustomers;
+    }
+
+    @Override
+    public void writeFooter(Writer writer) throws IOException {
+        writer.write("총 고객 수: " + aggregateCustomers.get("TOTAL_CUSTOMERS"));
+        writer.write(System.lineSeparator());
+        writer.write("총 나이: " + aggregateCustomers.get("TOTAL_AGES"));
+    }
+}
