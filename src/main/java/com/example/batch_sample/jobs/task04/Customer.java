0283e03c36d17f27989d1f9d855d046d258b1c07
HongYeseul <ghd8119@naver.com>
2024-10-26 13:11:51 +0900

    [4회차] csv 파일을 이용해 FlatFileItemReader 사용해보기 - 대상 Class 생성

---
+++ src/main/java/com/example/batch_sample/jobs/task04/Customer.java
@@ -0,0 +1,12 @@
+package com.example.batch_sample.jobs.task04;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+    private String name;
+    private int age;
+    private String gender;
+}
