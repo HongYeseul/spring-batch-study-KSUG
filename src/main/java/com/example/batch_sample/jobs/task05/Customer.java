64cd7c588eb70d8ab5c1250edac8dc02326290cc
HongYeseul <ghd8119@naver.com>
2024-11-02 16:40:06 +0900

    [5회차] JdbcPagingReader/Writer 실습을 위한 Customer 클래스 추가

---
+++ src/main/java/com/example/batch_sample/jobs/task05/Customer.java
@@ -0,0 +1,12 @@
+package com.example.batch_sample.jobs.task05;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private String name;
+    private int age;
+    private String gender;
+
+}
