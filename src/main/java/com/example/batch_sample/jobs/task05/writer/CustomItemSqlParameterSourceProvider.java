b700748069ce0ead9e5697e0bc89ca7058b486df
HongYeseul <ghd8119@naver.com>
2024-11-02 22:00:45 +0900

    [5회차] JdbcBatchItemWriter 실습용 SourceProvider 클래스

---
+++ src/main/java/com/example/batch_sample/jobs/task05/writer/CustomItemSqlParameterSourceProvider.java
@@ -0,0 +1,13 @@
+package com.example.batch_sample.jobs.task05.writer;
+
+import com.example.batch_sample.jobs.task05.Customer;
+import org.springframework.batch.item.database.ItemSqlParameterSourceProvider;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+public class CustomItemSqlParameterSourceProvider implements ItemSqlParameterSourceProvider<Customer> {
+    @Override
+    public SqlParameterSource createSqlParameterSource(Customer item) {
+        return new BeanPropertySqlParameterSource(item);
+    }
+}
