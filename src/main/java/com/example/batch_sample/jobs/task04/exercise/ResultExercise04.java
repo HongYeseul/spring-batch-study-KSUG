63e2577c3bc470e013737d7583f16dc1326c3813
HongYeseul <ghd8119@naver.com>
2024-11-04 21:06:10 +0900

    [4회차] 퀴즈 Writer용 클래스

---
+++ src/main/java/com/example/batch_sample/jobs/task04/exercise/ResultExercise04.java
@@ -0,0 +1,20 @@
+package com.example.batch_sample.jobs.task04.exercise;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class ResultExercise04 {
+
+    private String name;
+    private String month_1;
+    private String month_2;
+    private String month_3;
+    private String month_4;
+    private String month_5;
+    private String month_6;
+
+}
