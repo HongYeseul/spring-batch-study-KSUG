ea443a7a27d772ab7f0ebbbf2be0c448fa65eb0b
HongYeseul <ghd8119@naver.com>
2024-11-08 16:43:26 +0900

    [6회차] Customer Entity 클래스 추가

---
+++ src/main/java/com/example/batch_sample/jobs/task06/Customer.java
@@ -0,0 +1,26 @@
+package com.example.batch_sample.jobs.task06;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private Integer age;
+    private String gender;
+}
